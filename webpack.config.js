6ff55ab9ad4ac128056fd6c2f8cf34cf18718a0f
Cherkashin Alexander <cherkalexander@gmail.com>
2024-11-24 13:22:23 +0300

    add webpack manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
-const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: './src/lib.js',
   output: {
+    filename: "[name].[contenthash:8].js",
     library: {
         name: 'MyLibrary',
         type: 'umd'
     },
     clean: true
   },
+  plugins: [
+    new WebpackManifestPlugin({
+      fileName: 'asset-manifest.json',
+    })
+  ]
 };
